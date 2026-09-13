85d68e6189162eeb75031b203abc58230ed975bd
pau <pau.ramos@performanze.io>
2026-09-13 22:12:36 +0200

    feat: localize portfolio navigation

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,8 @@
 import type { MetadataRoute } from 'next'
 
 const origin = 'https://portfolio-pau-khaki.vercel.app'
-const paths = ['/', '/work/ovi', '/work/duplex', '/work/trustos', '/work/broki', '/foundations', '/foundations/cub3d', '/foundations/minishell', '/design', '/colophon']
+const paths = ['', '/work/ovi', '/work/duplex', '/work/trustos', '/work/broki', '/foundations', '/foundations/cub3d', '/foundations/minishell', '/design', '/colophon']
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return paths.map((path) => ({ url: origin + path, lastModified: new Date() }))
+  return ['en', 'es'].flatMap((locale) => paths.map((path) => ({ url: `${origin}/${locale}${path}`, lastModified: new Date() })))
 }
